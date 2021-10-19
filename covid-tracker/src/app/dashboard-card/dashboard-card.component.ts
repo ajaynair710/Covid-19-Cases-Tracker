f8fac4e19a78c14afdf9f6c56b040605b16ed6a0
Ajay Prakash Nair <42165927+ajaynair710@users.noreply.github.com>
2021-10-19 18:11:08 +0530

    added dashboard-card

--- covid-tracker/src/app/dashboard-card/dashboard-card.component.ts
+++ covid-tracker/src/app/dashboard-card/dashboard-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard-card',
@@ -6,6 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard-card.component.css']
 })
 export class DashboardCardComponent implements OnInit {
+  @Input('totalConfirmed')
+  totalConfirmed: any;
+  @Input('totalDeaths')
+  totalDeaths: any;
+  @Input('totalActive')
+  totalActive: any;
+  @Input('totalRecovered')
+  totalRecovered: any;
 
   constructor() { }
 
